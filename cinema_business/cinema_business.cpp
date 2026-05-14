60d89557cda2e487161042a837b86fe79759fd29
Ulvie Mustafa <UMMustafa23@codingburgas.bg>
2026-05-14 15:21:52 +0300

    Setup structure

---
+++ cinema_business/cinema_business.cpp
@@ -0,0 +1,10 @@
+// cinema_business.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fncinemabusiness()
+{
+}
